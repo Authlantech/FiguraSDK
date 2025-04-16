703ca02bff1028f13530ae70cc5b18f8a650ef38
ehank <emirhankotan140@gmail.com>
2025-04-16 14:43:28 +0300

    Redesigned README.md (not finished)

--- source/model.cpp
+++ source/model.cpp
@@ -155,7 +155,7 @@ bool Model::Load(const char* path)
 		
 		if (all_textures.find(diffusemapname_) != all_textures.end())
 		{
-			diffusemap = all_textures.find(diffusemapname_)._Ptr->_Myval.second;
+			diffusemap = all_textures.find(diffusemapname_)->second;
 		}
 
 		mesh.load(
